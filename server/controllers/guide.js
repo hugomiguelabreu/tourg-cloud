398a9e8fa3e3b2dd1197ace48eeccd0394e016c8
jpadrao <a76438@alunos.uminho.pt>
2018-11-30 15:50:54 +0000

    Added guide controller

---
+++ server/controllers/guide.js
@@ -0,0 +1,13 @@
+const Guide = require('../models').Guide;
+
+// create guide -> register
+exports.create_guide = function(req, res) {
+    return Guide
+        .create({
+            account_number: req.body.account_number,
+            swift: req.body.swift,
+            user_id: req.body.user_id
+        })
+        .then((user) => res.status(201).send(user))
+        .catch((error) => res.status(400).send(error));
+};
