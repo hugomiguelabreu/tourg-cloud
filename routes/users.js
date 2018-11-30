98cb2ed72bf9269821057498212125674c8ac90f
jpadrao <a76438@alunos.uminho.pt>
2018-11-30 11:42:30 +0000

    Added create user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const user_controller = require('../server/controllers/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// create user
+router.post('/create_user', user_controller.create_user);
+
+
 
 module.exports = router;
