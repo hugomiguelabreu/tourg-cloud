d68364a329a4b17eb2bb7c85b9a6edb18f4555c4
jpadrao <a76438@alunos.uminho.pt>
2018-11-30 17:38:49 +0000

    Added guide controller

--- server/controllers/guide.js
+++ server/controllers/guide.js
@@ -1,6 +1,8 @@
 const Guide = require('../models').Guide;
+const Activity = require('../models').Activity;
 
 // create guide -> register
+// TODO remove this endpoint ???
 exports.create_guide = function(req, res) {
     return Guide
         .create({
@@ -9,5 +11,17 @@ exports.create_guide = function(req, res) {
             user_id: req.body.user_id
         })
         .then((user) => res.status(201).send(user))
+        .catch((error) => res.status(400).send(error));
+};
+
+exports.create_activity = function(req, res) {
+    return Activity
+        .create({
+            description: req.body.description,
+            city: req.body.city,
+            date: req.body.date,
+            price: req.body.price
+        })
+        .then((act) => res.status(201).send(act))
         .catch((error) => res.status(400).send(error));
 };
