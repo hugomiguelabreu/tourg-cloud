c8b3f029884eb687b7b3116acd77ffb2b4efeb9f
jpadrao <a76438@alunos.uminho.pt>
2018-12-05 18:11:24 +0000

    Changed identation

--- server/models/activity.js
+++ server/models/activity.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Activity = sequelize.define('Activity', {
-    description: DataTypes.STRING,
-    city: DataTypes.STRING,
-    date: DataTypes.DATE,
-    price: DataTypes.DECIMAL(6,2)
-  }, {});
-  Activity.associate = function(models) {
-    Activity.belongsToMany(models.User, {through: 'Activity_Users', foreignKey: 'activity_id'});
-    Activity.hasMany(models.Activity_Evaluation, {foreignKey: 'activity_id'})
-  };
-  return Activity;
+    const Activity = sequelize.define('Activity', {
+        description: DataTypes.STRING,
+        city: DataTypes.STRING,
+        date: DataTypes.DATE,
+        price: DataTypes.DECIMAL(6,2)
+    }, {});
+    Activity.associate = function(models) {
+        Activity.belongsToMany(models.User, {through: 'Activity_Users', foreignKey: 'activity_id'});
+        Activity.hasMany(models.Activity_Evaluation, {foreignKey: 'activity_id'})
+    };
+    return Activity;
 };
--- server/models/credit_card.js
+++ server/models/credit_card.js
@@ -1,11 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Credit_Card = sequelize.define('Credit_Card', {
-    number: DataTypes.STRING,
-    expiry_date: DataTypes.DATEONLY
-  }, {});
-  Credit_Card.associate = function(models) {
-    Credit_Card.belongsTo(models['User'], {foreignKey: {allowNull: false, name:'user_id'}})
-  };
-  return Credit_Card;
+    const Credit_Card = sequelize.define('Credit_Card', {
+        number: DataTypes.STRING,
+        expiry_date: DataTypes.DATEONLY
+    }, {});
+    Credit_Card.associate = function(models) {
+        Credit_Card.belongsTo(models['User'], {foreignKey: {allowNull: false, name:'user_id'}})
+    };
+    return Credit_Card;
 };
