742eeebdac1b26fe25dc9f4530823e91049a1ba5
Bruno Renato Carvalho <brfc@MacBook-Pro-de-Bruno.local>
2018-12-13 11:45:19 +0000

    authentication added

--- app.js
+++ app.js
@@ -1,45 +1,84 @@
+var userTest =  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  };
+
+process.env.key= 'renatinholindo';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var guideRouter = require('./routes/guide');
+var bodyParser = require("body-parser");
+var jwt = require('jsonwebtoken');
+
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ 
+  extended: false }));
+
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({
+  extended: true}));
+
+app.use(bodyParser.json());
+
+/*=========  Authentication - Passport middleware configuration ========== */
+
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;         
+
+var jwtOptions = {}                                                       /*  Strategy Options    */
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = process.env.key; 
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload,next){    /*  Strategy Definition */
+      console.log('payload received', jwt_payload);
+      next(null, userTest);
+}); 
+
+
+passport.use(strategy);
+app.use(passport.initialize());
+/* ======================================================================== */
+
+
+/* =============================== ROUTES ================================= */
 app.use('/api/', indexRouter);
 app.use('/api/user', usersRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/guide', guideRouter);
+/* ======================================================================== */
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {                                        /* catch 404 and forward to error handler */
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
+app.use(function(err, req, res, next) {                                   /* error handler */
+  res.locals.message = err.message;     /* set locals, only providing error in development */
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500);                                  /* render the error page */
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 const user_controller = require('../server/controllers/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-// create user
-router.post('/create_user', user_controller.create_user);
 
-// add credit card to user
-// TODO check if credit card exists ???
+
+router.post('/register', user_controller.create_user);
+router.post('/login',user_controller.login);
 router.post('/add_credit_card', user_controller.add_credit_card);
-
 router.post('/add_activity', user_controller.add_activity);
-
 router.post('/send_message', user_controller.send_message);
-
 router.post('/add_complaint', user_controller.add_complaint);
-
 router.post('/evaluate_activity', user_controller.evaluate_activity);
-
 router.post('/evaluate_guide', user_controller.evaluate_guide);
 
+
+
 module.exports = router;
+
+/* ============ Comments ========== */
+
+/* Route that needs authentication, use example :
+router.get("/secret", passport.authenticate('jwt', { session: false }) ,function(req, res){
+        console.log("Key :" + process.env.key);
+
+});
+*/
