462c573054a686c5795c30ea55da646baef88cb1
jpadrao <a76438@alunos.uminho.pt>
2018-12-26 15:17:08 +0000

    Addded test route

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const api_controller = require('../server/controllers/api');
 
+router.get('/test', function (req, res) { // test route, nao remover pls
+    res.send('ping')
+});
 
 router.get('/activities', api_controller.activities);
 
