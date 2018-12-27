bcd5df3ca7f9e12c7fbc2fa1805fdaa493d049ca
jpadrao <a76438@alunos.uminho.pt>
2018-12-27 12:28:39 +0000

    Fixed scraper photos directory

--- scraper.js
+++ scraper.js
@@ -9,14 +9,47 @@ const n_places = 5;
 
 const request = require("request");
 const fs = require('fs');
+const path = require('path');
 const Place = require('./server/models').Place;
 const models = require('./server/models')
 
-const client_id = "KLNHDNOFVR5QN1JWA0KDA0HTQ0RVY0REMMICDU0MSISUTPHZ";
-const client_secret = "UDQUJRTYURYB044FTWJUQYDE0PSDGTOIM1RNV4K434IPVJZJ";
+const client_id = "KVJUEX4LGT3FQFD3DIQNHOBCL5SCL4NQCVCYKIUWJWPJ5COR";
+const client_secret = "V5B1K5UF44CJQVKPYDV520CV03YFSVFBDD3IW3HHLOLZTM2V";
 const base_url = "https://api.foursquare.com/v2/";
 
+function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
+    const sep = path.sep;
+    const initDir = path.isAbsolute(targetDir) ? sep : '';
+    const baseDir = isRelativeToScript ? __dirname : '.';
+
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = path.resolve(baseDir, parentDir, childDir);
+        try {
+            fs.mkdirSync(curDir);
+        } catch (err) {
+            if (err.code === 'EEXIST') { // curDir already exists!
+                return curDir;
+            }
+
+            // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+            if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
+                throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+            }
+
+            const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+            if (!caughtErr || caughtErr && curDir === path.resolve(targetDir)) {
+                throw err; // Throw if it's just the last created dir.
+            }
+        }
+
+        return curDir;
+    }, initDir);
+}
+
 function getLocations(){
+
+    mkDirByPathSync('/home/tourg/photos/');
+
         let queryString = {
             "client_id": client_id,
             "client_secret": client_secret,
@@ -49,7 +82,7 @@ function getLocations(){
                             if(data["response"]["venue"]["photos"]["groups"]){
                                let photo_url =  data["response"]["venue"]["photos"]["groups"][1]["items"]["0"]["prefix"] + "original"
                                     + data["response"]["venue"]["photos"]["groups"][1]["items"]["0"]["suffix"];
-                                let photo_path = "./photos/"+ data["response"]["venue"]["id"]+"_01"
+                                let photo_path = "/home/tourg/photos/"+ data["response"]["venue"]["id"]+"_01"
                                                 + data["response"]["venue"]["photos"]["groups"][1]["items"]["0"]["suffix"].substring(
                                                     data["response"]["venue"]["photos"]["groups"][1]["items"]["0"]["suffix"].length - 4);
                                 request.head(photo_url, 
