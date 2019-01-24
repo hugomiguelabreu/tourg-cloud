0b40b1d894ea19b219d2e8d8177075d1207aab44
jpadrao <a76438@alunos.uminho.pt>
2019-01-24 17:43:57 +0000

    Added admin functionalities

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const admin_controller = require('../server/controllers/admin');
 
 router.post('/create_guide', admin_controller.create_guide);
 
-// router.post('/add_category', api_controller.add_category);
+router.post('/add_cities', admin_controller.add_city);
+
+//router.post('/add_category', api_controller.add_category);
 
 module.exports = router;
